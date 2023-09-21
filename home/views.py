5a08fdbe32f14717f7a414f619fa477f7626ebcd
shamnad <hamzashamnad123@gmail.com>
2023-09-21 12:03:57 +0530

    views and url1

--- home/views.py
+++ home/views.py
@@ -43,6 +43,30 @@ def home(request):
 
 
 def productshow(request,prod_id,img_id):
+    variant=VariantImage.objects.filter(variant=img_id,is_available=True)
+    variant_images=(VariantImage.objects.filter(variant__product__id=prod_id,is_available=True).distinct('varianat_product'))
+    size=Size.objects.all(),
+    color=VariantImage.objects.filter(variant__product__id=prod_id,is_available=True).distinct('variant__color')
+
+    context={
+        'variant':variant,
+        'size':size,
+        'color':color,
+        'variant_images':variant_images,
+
+
+    }
+    return render(request,'user/product/productshow.html',context)
+
+
+def usercategoryshow(request,category_id):
+    variant=VariantImage.objects.filter(variant__product__category=category_id,is_available=True).distinct('variant_color')
+    context={
+        'variant':variant,
+
+    }
+    return render(request,'user/category/categoryuser.html',context)
+
 
 def blog(request):
     return render(request,'blog/blog.html')
