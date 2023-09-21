8b1ddfe688fe3afd614eb4b3e166fd77e897800e
shamnad <hamzashamnad123@gmail.com>
2023-09-21 11:05:53 +0530

    home

--- home/views.py
+++ home/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render,redirect
+import re
+from django.contrib import messages,auth
+from variant.models import VariantImage, Variant
+from django.db.models import Q 
+from datetime import datetime, timedelta
+from django.contrib.auth.decorators import login_required
+from product.models import Product,Size,Color
+from category.models import category
+
+
+
 
 
 
 
 def home(request):
-    return render(request,'user/home/index.html')
+    if  request.user.is_superuser:
+        return redirect('dashboard')
+    
+    categories=category.objects.all()
+    products=Product.objects.all()
+
+    # try:
+    #     cart_count=Cart.objects.filter(user=request.user).count()
+    #     whishlist_count=Wishlist.objects.filter(user=request.user).count()
+    # except:
+    #     cart_count=False
+    #     whishlist_count=False
+
+    variant_image=(VariantImage.objects.filter(variant__product__is_available=True).order_by('variant__product').distinct('variant__product'))
+    
+    context={
+        'categories':categories,
+        'products':products,
+        'variant_images':variant_image,
+        # 'wishlist_count':wishlist_count,
+        # 'cart_count':cart_count,
+    }
+
+    return render(request,'user/home/index.html',context)
+
+
+
+def productshow(request,prod_id,img_id):
 
 def blog(request):
     return render(request,'blog/blog.html')
