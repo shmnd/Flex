1a09afb7816dd4e3dcc4ae04dcb7f3e80f688edd
shamnad <hamzashamnad123@gmail.com>
2023-09-23 14:34:17 +0530

    image_back_to_first_settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,17 +132,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT='/flex/static/'
 
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS= [
-    os.path.join(BASE_DIR , 'static')
+    os.path.join(BASE_DIR / 'static')
 ]
 
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT =BASE_DIR ,'media'
+MEDIA_ROOT =BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
