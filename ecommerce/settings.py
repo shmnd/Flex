2520b989b9998deacb75aa527e05d5449ef52a68
Shamnad <hamzashamnad123@gmail.com>
2025-06-24 12:49:29 +0530

    sqlite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -96,15 +96,10 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('dbname'),
-        'USER': config('dbuser'),
-        'PASSWORD': config('dbpass'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Path to your SQLite database file
     }
-}  
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
