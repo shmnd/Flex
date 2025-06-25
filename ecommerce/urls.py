7ccea16a0f3cc6c38b4773c87459a80affe16180
Shamnad <hamzashamnad123@gmail.com>
2025-06-25 10:50:21 +0530

    CHANGES IN URL

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     
     
     
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
